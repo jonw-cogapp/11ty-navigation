e606df4e5385f22c985fc37266166a3074bcbdfd
jonw-cogapp <jonw@cogapp.com>
2020-08-07 18:18:46 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+
+const EleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(EleventyNavigationPlugin);
+
+	return {
+		pathPrefix: "/sdljfkaldsjlfka/"
+	}
+};
